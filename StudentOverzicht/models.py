cc6fa776b5bfa52082c2592bdef1678b6a1911de
DariusKlein <54933874+DariusKlein@users.noreply.github.com>
2020-09-07 13:34:26 +0200

    StudentOverzicht aangemaakt en test model gemaakt

--- StudentOverzicht/models.py
+++ StudentOverzicht/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BasisStudent (models.Model):
+    Student = models.TextField(max_length=30, default='')
+    Jaar = models.IntegerField(default=0)
+    Toets = models.IntegerField(default=0)
+    Behaald = models.BooleanField(default=False)
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'Testpage',
     'main',
+    'StudentOverzicht',
 ]
 
 MIDDLEWARE = [
