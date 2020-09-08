998a1548459b0f1aa23d208c7bc1d0ce341ecfe9
Darius <Darius.klein@student.hu.nl>
2020-09-08 11:38:31 +0200

    DB gemaakt naar ERD

--- StudentOverzicht/admin.py
+++ StudentOverzicht/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import BasisStudent
+from .models import Student, Cijfer, Toets, Blok
 
-admin.site.register(BasisStudent)
+admin.site.register(Student)
+admin.site.register(Cijfer)
+admin.site.register(Toets)
+admin.site.register(Blok)
