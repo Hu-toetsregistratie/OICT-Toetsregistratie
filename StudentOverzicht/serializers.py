50a83acf2248e074b5a112c47beb766e366551b1
dariu <Darius.klein@student.hu.nl>
2020-09-17 12:46:07 +0200

    Server update

--- StudentOverzicht/serializers.py
+++ StudentOverzicht/serializers.py
@@ -2,21 +2,16 @@ from rest_framework import serializers
 from .models import Student, Blok, Cijfer, Toets
 
 
-
 class Studentserializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
         fields = ('id', 'voornaam', 'achternaam')
 
 
-
 class Blokserializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Blok
         fields = ('id', 'blok')
-
-
-
 
 
 class Cijferserializer(serializers.HyperlinkedModelSerializer):
@@ -27,9 +22,6 @@ class Cijferserializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cijfer
         fields = ('id', 'cijfer', 'toets', 'blok', 'student')
-
-
-
 
 
 class Toetsserializer(serializers.HyperlinkedModelSerializer):
