08e37c803ec25b0bc4310dc20a9695b195015d0c
Darius <Darius.klein@student.hu.nl>
2020-10-07 15:54:49 +0200

    Filter

--- Main/views.py
+++ Main/views.py
@@ -12,7 +12,7 @@ class StudentList(viewsets.ModelViewSet):
     serializer_class = Studentserializer
     queryset = Student.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['voornaam', 'achternaam']
+    filterset_fields = ['id','voornaam', 'achternaam', 'student_nummer']
 
 
 
@@ -20,7 +20,8 @@ class StudentList(viewsets.ModelViewSet):
 class StudentDetail(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = Studentserializer
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id','voornaam', 'achternaam', 'student_nummer']
 
 
 ##############################################
@@ -28,38 +29,55 @@ class StudentDetail(viewsets.ModelViewSet):
 class BlokList(viewsets.ModelViewSet):
     queryset = Blok.objects.all()
     serializer_class = Blokserializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id','blok']
 
 class BlokDetail(viewsets.ModelViewSet):
     queryset = Blok.objects.all()
     serializer_class = Blokserializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id','blok']
 
 ##############################################
 class CijferList(viewsets.ModelViewSet):
     queryset = Cijfer.objects.all()
     serializer_class = Cijferserializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'voldoende', 'toets_code', 'blok', 'student']
 
 
 class CijferDetail(viewsets.ModelViewSet):
     queryset = Cijfer.objects.all()
     serializer_class = Cijferserializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'voldoende', 'toets_code', 'blok', 'student']
 
 ##############################################
 
 class ToetsList(viewsets.ModelViewSet):
     queryset = Toets.objects.all()
     serializer_class = Toetsserializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'toets_code', 'toets_naam', 'jaar', 'blok']
 
 
 class ToetsDetail(viewsets.ModelViewSet):
     queryset = Toets.objects.all()
     serializer_class = Toetsserializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'toets_code', 'toets_naam', 'jaar', 'blok']
+
 ###############################################
 class Cijfer_ID(viewsets.ModelViewSet):
     queryset = Cijfer.objects.all()
     serializer_class = Cijfer_ID_serializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'toets_code', 'blok', 'student','voldoende']
 
 
 
 class Toets_Full(viewsets.ModelViewSet):
     queryset = Toets.objects.all()
     serializer_class = Toets_Full
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'toets_code', 'toets_naam', 'jaar']
