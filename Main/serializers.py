222c916bc6138a44780dc16f7737e936769fa7f4
DariusKlein <54933874+DariusKlein@users.noreply.github.com>
2020-11-05 13:23:48 +0100

    Data gen + test klaar voor server

--- Main/serializers.py
+++ Main/serializers.py
@@ -27,7 +27,7 @@ class Cijferserializer(serializers.ModelSerializer): #serializer voor de Cijfer 
 
     class Meta:
         model = Cijfer
-        fields = ('id', 'voldoende', 'toets_code', 'blok', 'student', 'datum_toets')
+        fields = ('id', 'voldoende', 'toets_code', 'blok', 'student', 'datum_toets', "toets_naam")
 
 
 class Cijfer_ID_serializer(serializers.ModelSerializer): #serializer voor de Cijfer ID's
