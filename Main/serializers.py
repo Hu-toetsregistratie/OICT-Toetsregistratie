e16c143b66a9aaa4e587654936b3fdf94f7b3372
Darius <Darius.klein@student.hu.nl>
2020-11-18 09:52:44 +0100

    requirements.txt

--- Main/serializers.py
+++ Main/serializers.py
@@ -3,20 +3,20 @@ from rest_framework import serializers
 from .models import Student, Blok, Cijfer, Toets
 
 
-class Blokserializer(serializers.ModelSerializer): #serializer voor de blok api
+class Blokserializer(serializers.ModelSerializer):#serializer voor de blok api
     class Meta:
         model = Blok
         fields = ('id', 'blok', 'jaar')
 
 
-class Student_Naam(serializers.ModelSerializer): #serializer voor student in Cijfereserializer
+class Student_Naam(serializers.ModelSerializer):#serializer voor student in Cijfereserializer
 
     class Meta:
         model = Student
         fields = ('voornaam', 'achternaam', 'student_nummer')
 
 
-class Cijferserializer(serializers.ModelSerializer): #serializer voor de Cijfer api
+class Cijferserializer(serializers.ModelSerializer):#serializer voor de Cijfer api
     student = Student_Naam()
     toets_code = serializers.StringRelatedField()
     blok = serializers.StringRelatedField()
@@ -30,7 +30,7 @@ class Cijferserializer(serializers.ModelSerializer): #serializer voor de Cijfer 
         fields = ('id', 'voldoende', 'toets_code', 'blok', 'student', 'datum_toets', "toets_naam")
 
 
-class Cijfer_ID_serializer(serializers.ModelSerializer): #serializer voor de Cijfer ID's
+class Cijfer_ID_serializer(serializers.ModelSerializer):#serializer voor de Cijfer ID's
     student = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all())
     toets_code = serializers.PrimaryKeyRelatedField(queryset=Toets.objects.all())
     blok = serializers.PrimaryKeyRelatedField(queryset=Blok.objects.all())
@@ -40,10 +40,10 @@ class Cijfer_ID_serializer(serializers.ModelSerializer): #serializer voor de Cij
 
     class Meta:
         model = Cijfer
-        fields = ('id', 'voldoende', 'toets_code', 'blok', 'student', 'datum_toets', "toets_naam")
+        fields = ('id', 'voldoende', 'toets_code', 'blok', 'student', 'datum_toets',"toets_naam")
 
 
-class Toetsserializer(serializers.ModelSerializer): #serializer voor de Toetsen
+class Toetsserializer(serializers.ModelSerializer):#serializer voor de Toetsen
 
     blok = serializers.PrimaryKeyRelatedField(queryset=Blok.objects.all())
 
@@ -52,7 +52,7 @@ class Toetsserializer(serializers.ModelSerializer): #serializer voor de Toetsen
         fields = ('id', 'toets_code', 'toets_naam', 'jaar', 'blok')
 
 
-class Studentserializer(serializers.ModelSerializer): #serializer voor Studenten
+class Studentserializer(serializers.ModelSerializer):#serializer voor Studenten
 
     class Meta:
         model = Student
